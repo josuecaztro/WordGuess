44d871f32651dc445c71de97c59caa9a03873e39
Josue <josue@Zipcoders-MacBook-Pro-10.local>
2024-06-26 16:32:36 -0400

    plancomments

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -9,4 +9,34 @@ package com.github.zipcodewilmington;
 public class Hangman {
 
     //hello my Name is Josue!
+
+
+    //try to keep your methods small
+    //if methods get too big, break them up into smaller pieces with other methods
+    //DO GIT COMMITS ALONG THE WAY!!!
+
+
+        //FOR VERSION 1 OF YOUR GAME
+    //start the game
+    //make an array of words
+    //choose a random word from this array
+    //find how many letters are in the random word
+    //declare that you get x amount of tries from the amount of letters in step above
+    //set up a solution array
+    //set up an empty guesses array that the user will guess chars into
+
+    //set a while loop (while number of tries < tries allowed) AND
+    //the player has not guessed the word...
+    //print current state of player guesses, ask player for a new guess, if char = "-", quit the game,
+    //ELSE process the letter
+    //if the letter makes the word complete - PLAYER WINS
+
+
+    //after this while loop - if the word is not guessed - player loses.
+
+    /*
+    VERSION 2: add a while (true) conditional wrapped around the whole game code
+     that won't start unless they say Y/N to play again
+     */
+
 }
